f54d32c963729b861af746cadcf954d219c4a2e0
Denis Bushaev <denis.bushaev@ottofeller.com>
2022-11-23 18:24:40 +0400

    update mdx config in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,22 @@
+import nextMDX from '@next/mdx'
 import rehypeMathjax from 'rehype-mathjax'
 import remarkMath from 'remark-math'
 
+const withMDX = nextMDX({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeMathjax],
+    providerImportSource: '@mdx-js/react',
+  },
+})
+
 /** @type {import('next').NextConfig} */
-export default {
+export default withMDX({
   reactStrictMode: true,
   swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
   publicRuntimeConfig: {
     ...Object.fromEntries(Object.entries(process.env).filter(([key]) => key.startsWith('NEXT_PUBLIC_'))),
   },
-  webpack(config, options) {
-    config.module.rules.push({
-      test: /\.mdx?$/,
-      use: [
-        // The default `babel-loader` used by Next:
-        options.defaultLoaders.babel,
-        {
-          loader: '@mdx-js/loader',
-          /** @type {import('@mdx-js/loader').Options} */
-          options: {
-            remarkPlugins: [remarkMath],
-            rehypePlugins: [rehypeMathjax],
-            providerImportSource: '@mdx-js/react',
-          },
-        },
-      ],
-    })
-
-    return config
-  },
-}
+})
