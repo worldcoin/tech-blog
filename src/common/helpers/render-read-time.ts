2fb043b8706e65ffd521cada26faeda779a0ba2d
Denis Bushaev <denis.bushaev@ottofeller.com>
2022-11-30 16:40:31 +0400

    redesign article card

--- src/common/helpers/render-read-time.ts
+++ src/common/helpers/render-read-time.ts
@@ -1,2 +1,2 @@
 export const renderReadTime = (readTime: number, short = false) =>
-  readTime < 1 ? 'Less than minute read' : `${Math.round(readTime)}${short ? 'm' : ' minute'} read`
+  readTime < 1 ? `<1${short ? 'm' : ' minute'} read` : `${Math.round(readTime)}${short ? 'm' : ' minute'} read`
